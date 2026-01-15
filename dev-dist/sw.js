b5f8db0017c2732425a66facc636cc15fa8c25cb
Bhoj Nayak <bhojnayakacm@gmail.com>
2026-01-15 13:45:30 +0530

    fix(sw.js): correct revision number for index.html in service worker

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lnp6bp7122o"
+    "revision": "0.54mh4cnvgco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
